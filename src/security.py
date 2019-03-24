e6bff019458d18b6226a79724f25f49a5f57aa0f
francis <fc373745@gmail.com>
2019-03-24 09:58:51 -0700

    finished

--- src/security.py
+++ src/security.py
@@ -1,18 +1,15 @@
-from user import User
-
+import bcrypt
 from werkzeug.security import check_password_hash
 
-users = [
-    User(1, 'bob', 'asdf')
-]
+from resources.user import UserModel
 
 
 def authenticate(username, password):
-    user = User.find_by_username(username)
-    if user and check_password_hash(user.password, password):
+    user = UserModel.find_by_username(username)
+    if user and bcrypt.checkpw(password.encode('utf-8'), user.password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return User.find_by_id(user_id)
+    return UserModel.find_by_id(user_id)
